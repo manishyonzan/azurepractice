0e072e166be1081611270dc48391a6e6a0f6c997
Manish Lama <lmanish931@gmail.com>
2023-07-05 07:27:21 +0545

    third commit

---
+++ index.js
@@ -0,0 +1,15 @@
+    const http = require('http');
+
+    // const hostname = 'appservicew.azurewebsites.net';
+    const hostname = '20.119.0.33';
+    const port = 3000;
+
+    const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+    });
+
+    server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    });
