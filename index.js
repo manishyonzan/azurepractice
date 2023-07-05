f3bdd7a6ce2e1821cbf5fb135019014118864300
Manish Lama <lmanish931@gmail.com>
2023-07-05 16:38:07 +0545

    commit 8

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World!</h1>');
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-server.listen(port, 'localhost', () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
